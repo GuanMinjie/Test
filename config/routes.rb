557d134d42b9ea4efa3858119fc6dfdd26e06816
Minjie Guan <1163365485@qq.com>
2017-10-29 20:03:25 +0800

    修改路由配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  get 'login' => 'sessions#new'
+   get 'login' => 'sessions#new'
    post 'login' =>'sessions#create'
    delete 'logout' =>'sessions#destroy'
   resources :users
-
-  get'posts/:id',:to =>'posts#show',:as =>'show_post'
+  resources :posts
+  #get'posts/:id',:to =>'posts#show',:as =>'show_post'
   #post'posts/:id',:to =>'posts#show'
   
-  resources :posts do
+ # resources :posts do
      #collection do
-     get 'recent', :on => :collection
+   #  get 'recent', :on => :collection
      #end
-
-   end
+   #end
 #,:except =>:show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
